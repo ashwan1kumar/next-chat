9543a4685affe560143a0cb655775462c8fc8fd3
Ashwani <krashwani0908@gmail.com>
2024-11-29 06:38:51 +0530

    remove unused imports for build pipeline

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -2,7 +2,7 @@ import { ragChat } from "@/lib/rag-text";
 import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
     const {messages, sessionId} = await req.json();
     console.log(messages);
     const recentMessage = messages.at(-1).content;
--- src/lib/rag-text.ts
+++ src/lib/rag-text.ts
@@ -1,4 +1,4 @@
-import { RAGChat, ollama, upstash } from "@upstash/rag-chat"
+import { RAGChat, upstash } from "@upstash/rag-chat"
 import { redis } from "./redis"
 export const ragChat = new RAGChat({
     model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
